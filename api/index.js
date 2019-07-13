fdbdddb81e808bbe0e51e7df3deab260c3d7d081
joy.feng <614106721@qq.com>
2019-07-13 10:20:53 +0800

    猜歌名

---
+++ api/index.js
@@ -0,0 +1,1 @@
+"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getMusic=void 0;var _ajax=require("./ajax.js"),_ajax2=_interopRequireDefault(_ajax),getMusic=exports.getMusic=function(){return(0,_ajax2.default)("https://www.easy-mock.com/mock/5d1c66e9e85bc1461e567f67/api/music#!method=get")};
